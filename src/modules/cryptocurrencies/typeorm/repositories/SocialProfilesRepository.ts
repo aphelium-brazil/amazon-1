3097f050e87664e4d915618a33ef48681cf4e34f
gustavoabell <gustavoabel.contato@gmail.com>
2022-03-23 12:11:41 -0300

    refactor: resolve conflict on SocialProfiles

--- src/modules/cryptocurrencies/typeorm/repositories/SocialProfilesRepository.ts
+++ src/modules/cryptocurrencies/typeorm/repositories/SocialProfilesRepository.ts
@@ -1,6 +1,5 @@
-<<<<<<< HEAD:src/modules/cryptocurrencies/typeorm/repositories/SocialProfileRepository.ts
 import { ICreateSocialProfileDTO } from "@modules/cryptocurrencies/dtos/ICreateSocialProfileDTO";
-import { ISocialProfilesRepository } from "@modules/cryptocurrencies/repositories/ISocialProfilesRepository";
+import { ISocialProfilesRepository } from "@modules/cryptocurrencies/repositories/ISocialProfileRepository";
 import { getRepository, Repository } from "typeorm";
 import { SocialProfile } from "../entities/SocialProfile";
 
@@ -34,40 +33,3 @@ class SocialProfilesRepository implements ISocialProfilesRepository {
 }
 
 export { SocialProfilesRepository };
-=======
-import { ICreateSocialProfileDTO } from "@modules/cryptocurrencies/dtos/ICreateSocialProfileDTO";
-import { ISocialProfileRepository } from "@modules/cryptocurrencies/repositories/ISocialProfileRepository";
-import { getRepository, Repository } from "typeorm";
-import { SocialProfile } from "../entities/SocialProfile";
-
-class SocialProfilesRepository implements ISocialProfileRepository {
-    private repository: Repository<SocialProfile>;
-
-    constructor() {
-        this.repository = getRepository(SocialProfile);
-    }
-
-    async create({
-        name,
-        description,
-        url,
-        username,
-        socialNetwork,
-        official,
-    }: ICreateSocialProfileDTO): Promise<SocialProfile> {
-        const socialProfile = this.repository.create({
-            name,
-            description,
-            url,
-            username,
-            socialNetwork,
-            official,
-        });
-
-        await this.repository.save(socialProfile);
-        return socialProfile;
-    }
-}
-
-export { SocialProfilesRepository };
->>>>>>> cbd569d4d34495f4e74aba1188af41dd8e61ee8a:src/modules/cryptocurrencies/typeorm/repositories/SocialProfilesRepository.ts
