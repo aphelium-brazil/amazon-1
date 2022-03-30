9decb2f36f8f0ec2687bc5b139b1af1515725443
gustavoabell <gustavoabel.contato@gmail.com>
2022-03-29 21:51:07 -0300

    fix: resolve incorrect import in explorer-repo

--- src/modules/coin/typeorm/repositories/ExplorerRepository.ts
+++ src/modules/coin/typeorm/repositories/ExplorerRepository.ts
@@ -1,5 +1,6 @@
-import { ICreateExplorerDTO } from "@modules/cryptocurrencies/dtos/ICreateExplorerDTO";
-import { IExplorerRepository } from "@modules/cryptocurrencies/repositories/IExplorerRepository";
+
+import { ICreateExplorerDTO } from "@modules/coin/dtos/ICreateExplorerDTO";
+import { IExplorerRepository } from "@modules/coin/repositories/IExplorerRepository";
 import { getRepository, Repository } from "typeorm";
 import { Explorer } from "../entities/Explorer";
 
