2bf17bce367888ae3f4ead6dfaf9f4fc75e815f8
gustavoabell <gustavoabel.contato@gmail.com>
2022-04-06 11:10:48 -0300

    fix: import tag on interface of repository

--- src/modules/coin/repositories/interfaces/ITagsRepository.ts
+++ src/modules/coin/repositories/interfaces/ITagsRepository.ts
@@ -1,4 +1,5 @@
 import { ICreateTagDTO } from "@modules/coin/dtos/ICreateTagDTO";
+import { Tag } from "@modules/coin/entities/Tag";
 
 export interface ITagsRepository {
     create(data: ICreateTagDTO): Promise<Tag>;
